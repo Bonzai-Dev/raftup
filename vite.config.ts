f9251df1d520adc132fdd2f6d6c7bd06d5d8e6c5
Bonzai-Dev <137844567+Bonzai-Dev@users.noreply.github.com>
2025-04-24 23:41:16 -0400

    Support for vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import glsl from "vite-plugin-glsl";
+import vercel from "vite-plugin-vercel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), glsl()],
+  plugins: [tsconfigPaths(), glsl(), vercel()],
   optimizeDeps: {
     exclude: ["@babylonjs/havok"],
   },
   server: {
-    port: 3000,
+    port: process.env.PORT as unknown as number,
   },
 });
