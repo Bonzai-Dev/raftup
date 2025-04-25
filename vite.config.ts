6462fe315854d809345a5fa30db462fdc0ca2b17
Bonzai-Dev <137844567+Bonzai-Dev@users.noreply.github.com>
2025-04-24 23:45:08 -0400

    Moved assets to public directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import glsl from "vite-plugin-glsl";
-import vercel from "vite-plugin-vercel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), glsl(), vercel()],
+  plugins: [tsconfigPaths(), glsl()],
   optimizeDeps: {
     exclude: ["@babylonjs/havok"],
   },
   server: {
-    port: process.env.PORT as unknown as number,
+    port: 3000,
   },
 });
